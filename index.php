b5c64a826913290d7dadecd9bda9f664840ee89c
Joh <johan.grosset@timspirit.fr>
2016-05-20 10:20:46 +0200

    Test Sonar et Selenium

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <!-- Déclaration des normes et des caracteristiques de la page-->
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title> Bienvenue sur TimDevOps </title>
+<title> Bienvenue sur TiDevOps </title>
 <style type="text/css">
 /* Paramétrage du corps de la page */
 body{
@@ -26,7 +26,7 @@ p{
 	margin-right:auto;
 	height : 450px;
 	width: 710px; /* largeur obligatoire pour être centré */
-	background-color : <?php $couleur ="yellow";echo $couleur; ?>;
+	background-color : <?php $couleur ="white";echo $couleur; // Je commente à la fin du code même si je n'ai pas le droit?>;
 	border-radius: 10px;
 }
 #version{
