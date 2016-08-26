d5bd691c74549db5e702db4a0054e70504de9fda
Joh <johan.grosset@timspirit.fr>
2016-08-26 09:58:46 +0200

    Change color

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 </script>
  <style type="text/css">
 	body{font-family:"Roboto","Helvetica Neue",Helvetica,Arial,sans-serif;font-size:13px;line-height:1.846;
-	color:#666666;background-color:<?php $couleur ="yellow";echo $couleur;?>}
+	color:#666666;background-color:<?php $couleur ="white";echo $couleur;?>}
  </style>
   </head>
 
